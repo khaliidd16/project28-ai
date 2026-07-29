6dfe02a417d29b835171cf48053b7e79eefc5b46
unknown <khaledalali1998@gmail.com>
2026-07-30 01:15:22 +0200

    Update README for Day 1

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# project28-ai
-My journey to become an AI Automation Engineer
+# Project28
+
+My journey to become an AI Automation Engineer.
+
+## Day 1
+- Installed Python
+- Installed Git
+- Created GitHub repository
+- Built my first Python program
+- Learned variables, input, print and if statements
--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
-print("Hello Project28!")
-print("My journey to become an AI Automation Engineer starts today.")
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+print(f"Welcome {name}!")
+print(f"You are {age} years old")
+
+if age < "18":
+  print("You are still very young. Keep learning!")
+elif age > "18" and age < "30":
+  print("This is a great age to build your career!")
+else:
+  print("It's never too late to learn something new!")
+
+print("Welcome to Project28 🚀")
+print("Your goal is to become an AI Automation Engineer.")
+print("Good luck! 🚀")
