89b1f7a6ca3002d4b07ba45334763c076a3d7157
unknown <khaledalali1998@gmail.com>
2026-07-30 01:26:24 +0200

    Added age conditions

--- hello.py
+++ hello.py
@@ -1,15 +1,17 @@
 name = input("What is your name? ")
-age = input("How old are you? ")
+age = int(input("How old are you? "))
 
 print(f"Welcome {name}!")
 print(f"You are {age} years old")
 
-if age < "18":
-  print("You are still very young. Keep learning!")
-elif age > "18" and age < "30":
-  print("This is a great age to build your career!")
+if age == 100:
+    print("Wow! 100 years old! That's amazing! 🎉")
+elif age < 18:
+    print("You are still very young. Keep learning!")
+elif age < 30:
+    print("This is a great age to build your career!")
 else:
-  print("It's never too late to learn something new!")
+    print("It's never too late to learn something new!")
 
 print("Welcome to Project28 🚀")
 print("Your goal is to become an AI Automation Engineer.")
