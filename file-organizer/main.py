1f1c942772a1046ba7202f5cbc140cff94ca59e2
unknown <khaledalali1998@gmail.com>
2026-08-04 01:24:42 +0200

    Implement file sorting and ignore test files

--- file-organizer/main.py
+++ file-organizer/main.py
@@ -1,11 +1,46 @@
+import shutil
 from pathlib import Path
 
 folder = Path("test_files")
 
 files = folder.iterdir()
 
+images_folder = folder / "Images"
+images_folder.mkdir(exist_ok=True)
+
+documents_folder = folder / "Documents"
+documents_folder.mkdir(exist_ok=True)
+
+music_folder = folder / "Music"
+music_folder.mkdir(exist_ok=True)
+
 for file in folder.iterdir():
-    print(f"Name: {file.name}")
-    print(f"Stem: {file.stem}")
-    print(f"Extension: {file.suffix}")
-    print("----------------")
+    extension = file.suffix.lower()
+
+    if extension in [".jpg", ".jpeg", ".png"]:
+        print(f"{file.name} -> Images")
+        destination = images_folder / file.name
+        shutil.move(file, destination)
+        print(destination)
+
+    elif extension in [".mp3", ".wav"]:
+        print(f"{file.name} -> Music")
+        destination = music_folder / file.name
+        shutil.move(file, destination)
+        print(destination)
+
+
+    elif extension in [".mp4", ".mov"]:
+        print(f"{file.name} -> Videos")
+
+    elif extension in [".pdf", ".txt", ".docx"]:
+        print(f"{file.name} -> Documents")
+        destination = documents_folder / file.name
+        shutil.move(file, destination)
+        print(destination)
+
+    elif extension in [".zip", ".rar"]:
+        print(f"{file.name} -> Archives")
+
+    else:
+        print(f"{file.name} -> Other")
